45c88975faa590328c4109eb443f41da34de47b9
qudusayo <qqudusayo@gmail.com>
2021-10-26 19:26:37 +0100

    Fixed error logs

--- routes/Client/signup.js
+++ routes/Client/signup.js
@@ -6,13 +6,13 @@ const User = require("../../models/User.model");
 // const mail = require("../middleware/mailer");
 
 router.post("/", async (req, res) => {
-  let { firstName, lastName, email, phoneNumber, peerId, password } = req.body;
+  let { fullName, email, phoneNumber, peerId, password } = req.body;
 
   peerId ? (peerId = peerId.toLowerCase()) : null;
   email ? (email = email.toLowerCase()) : null;
 
   const newUser = new User({
-    name: `${firstName} ${lastName}`,
+    name: fullName,
     email,
     phoneNumber,
     peerId,
@@ -23,7 +23,7 @@ router.post("/", async (req, res) => {
 
   const checkEmail = await User.find({ email });
   if (!!checkEmail.length) {
-    return res.status(409).json({
+    return res.json({
       errorMsg: "Email already exist",
       error: true,
     });
@@ -31,7 +31,7 @@ router.post("/", async (req, res) => {
 
   const checkPeerId = await User.find({ peerId });
   if (!!checkPeerId.length) {
-    return res.status(409).json({
+    return res.json({
       errorMsg: "Peer-Id already exist",
       error: true,
     });
@@ -39,13 +39,14 @@ router.post("/", async (req, res) => {
 
   const checkPhoneNumber = await User.find({ phoneNumber });
   if (!!checkPhoneNumber.length) {
-    return res.status(409).json({
+    return res.json({
       errorMsg: "Phone Number already exist",
       error: true,
     });
   }
 
   newUser.save(function (err) {
+    console.log(err)
     if (err)
       return res.json({
         errorMsg: "Error registering user",
@@ -54,7 +55,7 @@ router.post("/", async (req, res) => {
       });
     // mail("welcome", email, { firstName });
     return res.status(200).json({
-      errorMsg: "Registration Successful, kindly verify your account",
+      errorMsg: "Registration Successful",
       error: false,
     });
   });
